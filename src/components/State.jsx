93854502ce3e853510bbcc61499f579ba688e947
nare6880 <nathan.t.reed@colorado.edu>
2023-11-15 20:15:11 -0500

    update rule, state interaction

--- src/components/State.jsx
+++ src/components/State.jsx
@@ -1,36 +1,44 @@
 import React, { useState } from "react";
 import Rule from "./rule";
 function State({ id, removeState }) {
-	const [rules, setRules] = useState({});
-	const addCase = () => {
-		let tempRule = { ...rules };
-		tempRule["default"] = null;
-		setRules(tempRule);
-	};
-	const updateRule = (state, action) => {
-		let tempRule = { ...rules };
-		tempRule[state] = action;
-		tempRule.default = "noDisplay";
-		console.log(tempRule);
-		setRules(tempRule);
-	};
-	return (
-		<div className="state">
-			<div className="state-header">
-				<h2>State {id}</h2>
-				<div className="state-control">
-					<button onClick={addCase}>Add case</button>
-					<button onClick={() => removeState(id - 1)}>Remove State</button>
-				</div>
-				{Object.keys(rules).map((key, index) => {
-					return rules[key] !== "noDisplay" ? (
-						<Rule key={key} data={rules[key]} updateRule={updateRule}></Rule>
-					) : (
-						""
-					);
-				})}
-			</div>
-		</div>
-	);
+  const [rules, setRules] = useState({ "default": "noDisplay" });
+  const addCase = () => {
+    let tempRule = { ...rules };
+    tempRule["default"] = "display";
+    setRules(tempRule);
+  };
+  const updateRule = (state, action) => {
+    let tempRule = { ...rules };
+    tempRule[state] = action;
+    tempRule.default = "noDisplay";
+    console.log(tempRule);
+    setRules(tempRule);
+  };
+  console.log(Object.keys(rules));
+  return (
+    <div className="state">
+      <div className="state-header">
+        <h2>State {id}</h2>
+        <div className="state-control">
+          <button onClick={addCase}>Add case</button>
+          <button onClick={() => removeState(id - 1)}>Remove State</button>
+        </div>
+        {Object.keys(rules).reverse().map((key, index) => {
+          console.log(key, index);
+          return rules[key] !== "noDisplay"
+            ? (
+              <Rule
+                locationRule={key}
+                data={rules[key]}
+                updateRule={updateRule}
+              />
+            )
+            : (
+              ""
+            );
+        })}
+      </div>
+    </div>
+  );
 }
 export default State;
--- src/components/rule.jsx
+++ src/components/rule.jsx
@@ -1,44 +1,59 @@
 import { React, useState } from "react";
+import Select from "react-select";
 import "../App.css";
-function Rule({ updateRule, key, data }) {
-	const [state, setState] = useState(key !== "default" ? key : "");
-	const [action, setAction] = useState(data !== "noDisplay" ? data : "");
-	return (
-		<div className="rule">
-			<p>case:</p>
-			<select
-				id="case"
-				onChange={(e) => {
-					if (e.target.value !== "" && action !== "") {
-						updateRule(state, action);
-					}
-					setState(e.target.value);
-				}}
-			>
-				<option value={""}>Select</option>
-				<option value={"Naaa"}>N***</option>
-				<option value={"aSaa"}>*S**</option>
-				<option value={"aaEa"}>**E*</option>
-				<option value={"aaaW"}>***W</option>
-			</select>
-			<p>action:</p>
-			<select
-				id="action"
-				onChange={(e) => {
-					if (state !== "" && e.target.value !== "") {
-						updateRule(state, e.target.value);
-					}
-					setAction(e.target.value);
-				}}
-			>
-				<option value={""}>Select</option>
-				<option value={"N"}>N</option>
-				<option value={"S"}>S</option>
-				<option value={"E"}>E</option>
-				<option value={"W"}>W</option>
-			</select>
-		</div>
-	);
+
+function Rule({ updateRule, locationRule, data }) {
+  const [state, setState] = useState(locationRule);
+  const [action, setAction] = useState(data);
+  console.log("key: ", locationRule, "data:", data);
+  const stateOptions = [
+    { value: "default", label: "Select" },
+    { value: "Naaa", label: "N***" },
+    { value: "aSaa", label: "*S**" },
+    { value: "aaEa", label: "**E*" },
+    { value: "aaaW", label: "***W" },
+  ];
+  const actionOptions = [
+    { value: "display", label: "Select" },
+    { value: "N", label: "N" },
+    { value: "S", label: "S" },
+    { value: "E", label: "E" },
+    { value: "W", label: "W" },
+  ];
+
+  return (
+    <div className="rule">
+      <p>case:</p>
+      <Select
+        value={stateOptions.value}
+        options={stateOptions}
+        onChange = {(value, actionType) => {
+          console.log(value.value)
+          if(value !=="default" && action !=="display"){
+            updateRule(value.value,action)
+          }
+          setState(value.value)
+        }}
+        defaultValue={stateOptions[
+          stateOptions.findIndex((obj) => obj.value === state)
+        ]}
+      />
+      <p>action:</p>
+      <Select
+        value={actionOptions.value}
+        options={actionOptions}
+        defaultValue={actionOptions[
+          actionOptions.findIndex((obj) => obj.value === data)
+        ]}
+        onChange = {(value, actionType) => {
+          if(value !=="display" && state !=="default"){
+            updateRule(state,value.value)
+          }
+          setAction(value.value)
+        }}
+      />
+    </div>
+  );
 }
 
 export default Rule;
