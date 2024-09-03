644a6d943fd907e98bf0c1dda43d259582bb5d09
Nare6880 <49699188+Nare6880@users.noreply.github.com>
2024-09-02 18:22:50 -0600

    Add webAnalytics

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import PicoScreen from "./components/PicoScreen";
 import PicoControl from "./components/PicoControl";
 import React, { useEffect } from "react";
 import { SpeedInsights } from "@vercel/speed-insights/react";
+import { inject } from "@vercel/analytics";
 import { useState } from "react";
 import "./App.css";
 import { Provider } from "react-redux";
@@ -24,6 +25,7 @@ function App() {
 	useEffect(() => {
 		document.title = "PicoBot";
 	});
+	inject();
 	return (
 		<div className="App">
 			<Provider store={store}>
